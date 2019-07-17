d65f9525ff66c723e723a73eb2f9925cf92c2296
Olivia Kesler <oakesler@protonmail.ch>
2019-07-17 04:05:34 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(collection)
+  n = collection.length
+  if block_given?
+    i = 0 
+    while i < n 
+      yield collection[i]
+      i += 1
+   end
+ end
+ collection
+end 
